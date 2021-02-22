3bc9aa78b3ff1ff1d6188f7d025c2de23f986f61
yuxt <yuxt@live.com>
2021-02-22 23:00:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>d3 sample</h1>
+  <d3-chart></d3-chart>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+import d3Chart from "./components/d3Chart.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    d3Chart
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 10px auto;
+  max-width: 720px;
+  padding-top: 10px;
+}
+</style>
